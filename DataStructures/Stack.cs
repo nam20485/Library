2a585f5b25227c36ae1e16e99c07080e9e73403f
Nathan Miller <nmiller217@gmail.com>
2023-08-20 15:47:09 -0700

    implemented Stack

--- DataStructures/Stack.cs
+++ DataStructures/Stack.cs
@@ -1,12 +1,100 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Collections;
 
 namespace Library.DataStructures
 {
-    internal class Stack
+    public class Stack<TValue> : IEnumerable<TValue>
     {
+        private readonly List<TValue> _items;        
+
+        public int Count => _items.Count;
+
+        public bool IsReadOnly => false;
+
+        public Stack()
+        {
+            _items = new List<TValue>();
+        }
+
+        public Stack(IEnumerable<TValue> collection)
+            : this()
+        {
+            AddRange(collection);
+        }      
+
+        public bool IsEmpty()
+        {
+            return Count == 0;
+        }
+
+        public void Push(TValue value)
+        {
+            Add(value);
+        }
+
+        public TValue Pop()
+        {            
+            if (!IsEmpty())
+            {
+                var item = Peek();
+                _items.RemoveAt(Count - 1);
+                return item;
+            }
+            throw new InvalidOperationException("Stack is empty");
+        }
+
+        public TValue Peek()
+        {     
+            if (!IsEmpty())
+            {
+                return _items[Count - 1];
+            }
+            throw new InvalidOperationException("Stack is empty");
+        }
+
+        public void Add(TValue item)
+        {
+            _items.Add(item);
+        }
+
+        public void Clear()
+        {
+            _items.Clear();
+        }
+
+        public bool Contains(TValue item)
+        {
+            return _items.Contains(item);
+        }
+
+        public void CopyTo(TValue[] array, int arrayIndex)
+        {
+            foreach (var item in _items)
+            {
+                array[arrayIndex++] = item;
+            }
+        }      
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+
+        public IEnumerator<TValue> GetEnumerator()
+        {
+            var index = Count - 1;
+            while (index >= 0)
+            {
+                yield return _items[index];
+            }
+        }
+
+        public void AddRange(IEnumerable<TValue> collection)
+        {
+            foreach (var item in collection)
+            {
+                Add(item);
+            }
+        }
+
     }
 }
