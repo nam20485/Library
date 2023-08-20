76e2bb0105e7da5318906c4b87ab72f9e6a4732a
Nathan Miller <nmiller217@gmail.com>
2023-08-20 13:46:02 -0700

    add Driver console app

---
+++ Driver/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace Library.Driver
+{
+    internal class Program
+    {
+        private static readonly int[][] testCases = new[]
+        {
+            //Array.Empty<int>(),
+            new int[] { 0 },
+            new int[] { 0, 1 },
+            new int[] { 0, 1, 2 },
+            new int[] { 0, 1, 2, 3 }
+        };
+
+        static void Main(/*string[] args*/)
+        {
+            foreach (var testCase in testCases)
+            {
+                Test(testCase);
+            }         
+        }
+
+        private static void Test(int[] items)
+        {
+            var linkedList = new DataStructures.LinkedList<int>(items);
+            Console.WriteLine(linkedList);
+
+            linkedList.RemoveLast();
+            linkedList.RemoveFirst();
+            Console.WriteLine(linkedList);
+            Console.WriteLine();
+        }
+    }
+}
