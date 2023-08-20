53aab9e54c8b8d673cade4bb4c4d78d0574d3aa9
Nathan Miller <nmiller217@gmail.com>
2023-08-20 13:46:19 -0700

    add some tests

---
+++ Tests/ICollectionTests.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Library.Tests
+{
+    public interface ICollectionTests
+    {
+        //[Theory]
+        //[InlineData(new int[] { 0 })]
+        //public void TestCollectionInterface(ICollection<int> collection)
+        //{
+        //}
+    }
+}
---
+++ Tests/LinkedListTests.cs
@@ -0,0 +1,21 @@
+﻿namespace Library.Tests
+{
+    public class LinkedListTests
+    {
+        [Theory]
+        [InlineData(new int[] { })]
+        [InlineData(new int[] { 0 })]
+        [InlineData(new int[] { 0, 1 })]
+        [InlineData(new int[] { 0, 1, 2 })]
+        [InlineData(new int[] { 0, 1, 2, 3 })]
+        public void Test_ToString(int[] items)
+        {
+            var linkedList = new LinkedList<int>(items);
+            var str = linkedList.ToString();
+            Console.WriteLine(str);
+            str.Should().NotBeNull();
+            str = str!.Trim();
+            str.Should().NotBeEmpty();
+        }
+    }
+}
