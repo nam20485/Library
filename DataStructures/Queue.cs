29773d1035d2d86459d45770a73ea3e1a3b0f363
Nathan Miller <nmiller217@gmail.com>
2023-08-20 15:26:16 -0700

    add methods to Queue class

--- DataStructures/Queue.cs
+++ DataStructures/Queue.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,73 @@ using System.Threading.Tasks;
 
 namespace Library.DataStructures
 {
-    internal class Queue
+    public class Queue<TValue> : ICollection<TValue>
     {
+        public int Count => throw new NotImplementedException();
+
+        public bool IsReadOnly => throw new NotImplementedException();
+
+        public Queue()
+        {
+
+        }
+
+        public Queue(IEnumerable<TValue> collection)
+        {
+            foreach (var item in collection)
+            {
+                Add(item);
+            }
+        }
+
+        public void Enqueue(TValue value)
+        {
+            Add(value);
+        }
+
+        public TValue Dequeue()
+        {
+            throw new NotImplementedException();
+        }
+
+        public TValue Peek()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Add(TValue item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Clear()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Contains(TValue item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void CopyTo(TValue[] array, int arrayIndex)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerator<TValue> GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Remove(TValue item)
+        {
+            throw new NotImplementedException();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }
     }
 }
