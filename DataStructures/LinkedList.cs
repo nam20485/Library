6fb3734d0cb87bd9c903275357c46da93991d65b
Nathan Miller <nmiller217@gmail.com>
2023-08-20 13:44:20 -0700

    LinkedList working

--- DataStructures/LinkedList.cs
+++ DataStructures/LinkedList.cs
@@ -7,45 +7,308 @@ using System.Threading.Tasks;
 
 namespace Library.DataStructures
 {
-    public class LinkedList<TItem> : ICollection<TItem>
+    public class LinkedList<TValue> : ICollection<TValue>
     {
-        public int Count => throw new NotImplementedException();
+        private Node? _head;
+        private Node? _tail;
+        private int _count;
+
+        public Node? First => _head;
+        public Node? Last => _tail;
+
+        public LinkedList()
+        {
+            _head = null;
+            _tail = null;
+            _count = 0;
+        }
+
+        public LinkedList(IEnumerable<TValue> collection)
+        {
+            foreach (var item in collection)
+            {
+                Add(item);
+            }
+        }
+
+        //
+        //  ICollection interface implementation
+        //
+        public int Count => _count;
 
         public bool IsReadOnly => throw new NotImplementedException();
 
-        public void Add(TItem item)
+        /// <summary>
+        /// Add value to end of the list
+        /// </summary>
+        /// <param name="value">Item to add</param>
+        /// <exception cref="NotImplementedException"></exception>
+        public void Add(TValue value)
         {
-            throw new NotImplementedException();
+            AddAfter(Last, value);
         }
 
         public void Clear()
         {
-            throw new NotImplementedException();
+            _head = null;
+        }        
+
+        public bool Contains(TValue value)
+        {
+            return Find(value) != null;
         }
 
-        public bool Contains(TItem item)
+        public void CopyTo(TValue[] array, int arrayIndex)
         {
-            throw new NotImplementedException();
+            var index = arrayIndex;
+            foreach (var item in this)
+            {
+                array[index++] = item;
+            }
+        }              
+
+        public bool Remove(TValue item)
+        {
+            var toRemove = Find(item);
+            return RemoveNode(toRemove);
         }
 
-        public void CopyTo(TItem[] array, int arrayIndex)
+        //
+        //  More LinkedList interface
+        //
+        public void AddLast(TValue value)
         {
-            throw new NotImplementedException();
+            Add(value);
         }
 
-        public IEnumerator<TItem> GetEnumerator()
+        public void AddFirst(TValue value)
         {
-            throw new NotImplementedException();
+            AddBefore(First, value);            
         }
 
-        public bool Remove(TItem item)
+        public void AddAfter(Node? node, TValue value)
         {
-            throw new NotImplementedException();
+            var newNode = new Node(value)
+            {
+                Previous = node
+            };
+
+            if (node != null)
+            {                
+                newNode.Next = node.Next;
+                if (node.Next != null)
+                {
+                    node.Next.Previous = newNode;
+                }
+                else
+                {
+                    // we added after the current _tail, new _tail is the new node
+                    _tail = newNode;
+                }
+                node.Next = newNode;
+            }
+            else
+            {
+                _head = newNode;
+                _tail = newNode;
+            }
+            _count++;
+        }
+
+        public void AddBefore(Node? node, TValue value)
+        {
+            var newNode = new Node(value)
+            {
+                Next = node               
+            };
+
+            if (node != null)
+            {
+                newNode.Previous = node.Previous;
+                node.Previous = newNode;
+                if (node.Next != null)
+                {
+                    node.Next.Previous = newNode;
+                }
+                if (newNode.Previous == null)
+                {
+                    // we added before the head, newNode is the new _head
+                    _head = newNode;
+                }
+            }
+            else
+            {
+                _head = newNode;
+                _tail = newNode;
+            }
+            _count++;
+        }
+
+        public void RemoveFirst()
+        {
+            RemoveNode(First);
+        }
+
+        public void RemoveLast()
+        {
+            RemoveNode(Last);
+        }
+
+        //
+        //  Private implementation
+        //
+        public Node? Find(TValue value)
+        {
+            var current = _head;
+            while (current != null)
+            {
+                if (Equals(current.Value, value))
+                {
+                    break;
+                }
+                current = current.Next;
+            }
+            return current;
+        }
+
+        private bool RemoveNode(Node? node)
+        {
+            if (node != null)
+            {
+                var prevNode = node.Previous;
+                var nextNode = node.Next;
+                if (prevNode != null)
+                {
+                    prevNode.Next = nextNode;
+                }
+                else
+                {
+                    _head = nextNode;
+                }
+                if (nextNode != null)
+                {
+                    nextNode.Previous = prevNode;
+                }
+                else
+                {
+                    _tail = prevNode;
+                }
+
+                _count--;
+                return true;
+            }
+            else
+            {
+                return false;
+            }        
+
+            //if (node != null)
+            //{
+            //    // node is head
+            //    if (node.Previous == null)
+            //    {
+            //        if (node.Next != null)
+            //        {
+            //            // there are nodes after _head, i.e. Count > 1
+            //            node.Next.Previous = null;
+            //        }
+            //        // make the next node the head (if only 1 element was in the list, then _head = null now)
+            //        _head = node.Next;
+            //        _tail = node.Next;
+            //    }
+            //    else
+            //    {
+            //        // node is not head
+
+            //    }
+            //}
+            //return false;
+        }
+
+        //
+        //  IEnumerable interface implementation
+        //
+        public IEnumerator<TValue> GetEnumerator()
+        {
+            var current = _head;
+            while (current != null)
+            {
+                yield return current.Value;
+                current = current.Next;
+            }
         }
 
         IEnumerator IEnumerable.GetEnumerator()
         {
-            throw new NotImplementedException();
+            return GetEnumerator();
+        }
+
+        public override string ToString()
+        {
+            var sb = new StringBuilder();
+
+            var current = First;
+            while (current != null)
+            {
+                sb.Append(current.ToString());
+                current = current.Next;
+            }
+            sb.Append($"  ({_count})");
+            sb.AppendLine();
+
+            var head = 0;
+            current = First;
+            while (current != First && current != null)
+            {
+                head++;
+                current = current.Next;
+            }
+            //sb.Append($"{new string(' ', head)}^");        
+
+            var tail = 0;
+            current = First;
+            while (current != Last && current != null)
+            {
+                tail++;
+                current = current.Next;
+            }
+            //sb.Append($"{new string(' ', (tail-head)*5-1)}^");        
+
+            //sb.AppendLine();
+            sb.Append($"{new string(' ', head)}h");
+            var tailPos = (tail - head) * 5 - 1;
+            sb.Append($"{new string(' ', tailPos > 0? tailPos : 0)}t");            
+
+            return sb.ToString();
+        }
+
+        public class Node
+        {
+            public TValue Value { get; }
+            public Node? Next { get; internal set; }    // only Library can set Next and Previous, not user
+            public Node? Previous { get; internal set; }
+
+            public Node(TValue value)
+            {
+                Value = value;
+                Next = null;
+                Previous = null;
+            }
+
+            public override string ToString()
+            {
+                var sb = new StringBuilder();
+                if (Previous != null)
+                {
+                    sb.Append("<-");
+                }
+                sb.Append(Value);
+                if (Next != null)
+                {
+                    sb.Append("->");
+                }
+                return sb.ToString();
+            }
         }
     }
 }
