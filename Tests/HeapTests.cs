aba77c49908bdf09be2cb77bc2590b735e1857aa
Nathan Miller <nmiller217@gmail.com>
2023-08-21 10:03:37 -0700

    add some tests

---
+++ Tests/HeapTests.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Library.DataStructures;
+
+namespace Library.Tests
+{
+    public class HeapTests
+    {
+        [Fact]
+        public void Test_Heap()
+        {
+            var testCases = new[]
+            {
+                new [] { 4, 1, 3, 2, 16, 9, 10, 14, 8, 7 }
+            };
+
+            foreach (var testCase in testCases)
+            {
+                var heap = new Heap<int>(testCase);
+                heap.ToString();
+            }
+        }
+    }
+}
