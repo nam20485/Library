2d61343b08633b62d24e5619ccdeede63f68b861
Nathan Miller <nmiller217@gmail.com>
2023-08-21 13:08:25 -0700

    add a static Instance member to use when many repeated instantiations would keep making new copies

--- DataStructures/ReverseDefaultComparer.cs
+++ DataStructures/ReverseDefaultComparer.cs
@@ -2,6 +2,11 @@
 {
     public class ReverseDefaultComparer<T> : Comparer<T>
     {
+        /// <summary>
+        /// Static instnace to use if you need avoid repeated instantiations of new copies.
+        /// </summary>
+        public static readonly Comparer<T> Instance = new ReverseDefaultComparer<T>();
+
         public override int Compare(T? x, T? y)
         {
             return -Default.Compare(x, y);
