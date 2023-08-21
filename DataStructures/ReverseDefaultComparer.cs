4b2cabc8c2c9f1e7ebee48d86deb40d205434e92
Nathan Miller <nmiller217@gmail.com>
2023-08-21 12:49:25 -0700

    add comparer class to allow for descending sort

---
+++ DataStructures/ReverseDefaultComparer.cs
@@ -0,0 +1,10 @@
+﻿namespace Library.DataStructures
+{
+    public class ReverseDefaultComparer<T> : Comparer<T>
+    {
+        public override int Compare(T? x, T? y)
+        {
+            return -Default.Compare(x, y);
+        }
+    }
+}
