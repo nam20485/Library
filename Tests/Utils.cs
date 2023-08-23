9ee7eba42d6b51a33e4b974bd2aff5152bbfe9ce
Nathan Miller <nmiller217@gmail.com>
2023-08-23 11:46:03 -0700

    add Utils class

---
+++ Tests/Utils.cs
@@ -0,0 +1,17 @@
+﻿using Library.DataStructures;
+
+namespace Library.Tests
+{
+    internal class Utils
+    {
+        public static IEnumerable<IDsCollection<TInput>> MakeIDsCollections<TInput>(TInput[] inputs)
+        {
+            return new IDsCollection<TInput>[]
+            {
+                new DataStructures.List<TInput>(inputs),
+                new DataStructures.LinkedList<TInput>(inputs),
+                new DataStructures.Heap<TInput>(inputs)
+            };
+        }
+    }
+}
