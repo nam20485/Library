e8ea94fad16a1dda35baa451e014a7dee4cd55f4
Nathan Miller <nmiller217@gmail.com>
2023-08-22 17:03:12 -0700

    changes

--- Driver/Program.cs
+++ Driver/Program.cs
@@ -1,33 +1,61 @@
-﻿namespace Library.Driver
+﻿using Library.DataStructures;
+
+namespace Library.Driver
 {
     internal class Program
     {
         private static readonly int[][] testCases = new[]
         {
-            //Array.Empty<int>(),
-            new int[] { 0 },
-            new int[] { 0, 1 },
-            new int[] { 0, 1, 2 },
-            new int[] { 0, 1, 2, 3 }
+            new [] { 4, 1, 3, 2, 16, 9, 10, 14, 8, 7 },
+            Array.Empty<int>(),
+            new [] { 0 },
+            new [] { 0, 1 },
+            new [] { 0, 1, 2 },
+            new [] { 0, 1, 2, 3 }
         };
 
         static void Main(/*string[] args*/)
         {
             foreach (var testCase in testCases)
             {
-                Test(testCase);
+                //TestLinkedList(testCase);
+                TestHeap(testCase);
+                //TestList(testCase);
             }         
         }
 
-        private static void Test(int[] items)
+        private static void TestLinkedList(int[] items)
         {
             var linkedList = new DataStructures.LinkedList<int>(items);
             Console.WriteLine(linkedList);
+            var heap = new DataStructures.Heap<int>(items);
+            Console.WriteLine(heap);
 
             linkedList.RemoveLast();
             linkedList.RemoveFirst();
             Console.WriteLine(linkedList);
             Console.WriteLine();
         }
+
+        private static void TestHeap(int[] items)
+        {
+            var heap = new Heap<int>(items, ReverseDefaultComparer<int>.Instance);
+            //heap.Sort();
+            ////var a = heap.ToArray<int>();
+            ////var l = heap.ToList<int>();
+            //var alal = new DataStructures.List<int>(heap.ToList<int>().ToArray());
+            Console.WriteLine(heap);
+            //Console.WriteLine(alal);
+            //var sorted = Heap<int>.HeapSort(items, ReverseDefaultComparer<int>.Instance);
+            //Console.WriteLine(new DataStructures.List<int>(sorted));
+        }
+
+        private static void TestList(int[] items)
+        {
+            var list = new DataStructures.List<int>(items);
+            Console.WriteLine(list);
+
+            //list.Add
+        }        
     }
 }
