46357705015af75be3fe6cd6d5f667b2e3fce058
Nathan Miller <nmiller217@gmail.com>
2023-08-23 18:42:44 -0700

    add tests for MinHeap

--- Tests/HeapTests.cs
+++ Tests/HeapTests.cs
@@ -28,13 +28,51 @@ namespace Library.Tests
 
         [Theory]
         [InlineData(new[] { 4, 1, 3, 2, 16, 9, 10, 14, 8, 7 })]
+        public void ValidMinHeapIsCreatedWithInputs(int[] inputs)
+        {
+            foreach (var testCase in inputs)
+            {
+                var minHeap = new MinHeap<int>(inputs);
+                minHeap.IsHeap().Should().BeTrue();
+                minHeap.SatisfiesHeapProperty().Should().BeTrue();
+            }
+        }
+
+        [Theory]
+        [InlineData(new[] { 4, 1, 3, 2, 16, 9, 10, 14, 8, 7 })]
         public void HeapSortIsSortedCorrectly(int[] inputs)
         {
             foreach (var testCase in inputs)
             {
                 var heap = new Heap<int>(inputs);
-                
+                heap.Sort();                    
+                heap.ToList().IsSorted(DataStructures.List<int>.SortType.MonotonicallyDecreasing).Should().BeTrue();                
             }
         }
+
+        [Theory]
+        [InlineData(new[] { 4, 1, 3, 2, 16, 9, 10, 14, 8, 7 })]
+        public void HeapSortIsSortedCorrectly_ReverseComparer(int[] inputs)
+        {
+            foreach (var testCase in inputs)
+            {
+                var heap = new Heap<int>(inputs, ReverseDefaultComparer<int>.Instance);
+                heap.Sort();            
+                heap.ToList().IsSorted(DataStructures.List<int>.SortType.MonotonicallyIncreasing).Should().BeTrue();
+            }
+        }
+
+        [Theory]
+        [InlineData(new[] { 4, 1, 3, 2, 16, 9, 10, 14, 8, 7 })]
+        public void MinHeapSortIsSortedCorrectly(int[] inputs)
+        {
+            foreach (var testCase in inputs)
+            {
+                var minHeap = new MinHeap<int>(inputs);
+                minHeap.Sort();
+                minHeap.ToList().IsSorted(DataStructures.List<int>.SortType.MonotonicallyIncreasing).Should().BeTrue();
+            }
+        }
+
     }
 }
