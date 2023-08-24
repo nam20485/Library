922ede2db66156162fd575e8a1ce09b9e7243c98
Nathan Miller <nmiller217@gmail.com>
2023-08-23 17:48:24 -0700

    add MakeList

--- Tests/Utils.cs
+++ Tests/Utils.cs
@@ -13,5 +13,14 @@ namespace Library.Tests
                 new DataStructures.Heap<TInput>(inputs)
             };
         }
+
+        public static DataStructures.List<int> MakeList(int[] inputs)
+        {
+            var list = new DataStructures.List<int>(inputs);
+            list.Should().NotBeNull();
+            list.Should().NotBeEmpty();
+            list.Should().HaveCount(inputs.Length);
+            return list;
+        }
     }
 }
