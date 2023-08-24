3b2c0eb2caffefd2d8c2cbce24556c35e2eb77e4
Nathan Miller <nmiller217@gmail.com>
2023-08-24 13:06:14 -0700

    put test data generation classes in their own classes

---
+++ Tests/IdsCollectionsTestData.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+namespace Library.Tests
+{
+    public abstract class IDsCollectionsTestData<TInput> : IEnumerable<object[]>
+    {
+        protected abstract TInput[][] Inputs { get; }
+
+        //protected IDsCollection<TInput>[] IDsCollections => 
+        //    new IDsCollection<TInput>[]
+        //        {                    
+        //            new DataStructures.List<TInput>(),                    
+        //            new DataStructures.LinkedList<TInput>(),
+        //            new Heap<TInput>(),
+        //        };        
+
+        public IEnumerator<object[]> GetEnumerator()
+        {
+            //foreach (var iDsCollection in IDsCollections)
+            //{
+            //    foreach (var inputs in Inputs)
+            //    {
+            //        yield return new object[] { iDsCollection, inputs };
+            //    }
+            //}
+            foreach (var inputs in Inputs)
+            {
+                yield return new object[] { new DataStructures.List<TInput>(), inputs };
+                yield return new object[] { new DataStructures.LinkedList<TInput>(), inputs };
+                yield return new object[] { new DataStructures.Heap<TInput>(), inputs };
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+    }
+}
---
+++ Tests/IdsCollectionsTestDatas.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Library.Tests
+{
+    public class IntIDsCollectionTestData : IDsCollectionsTestData<int>
+    {
+        protected override int[][] Inputs => new[]
+            {
+                new [] { 4, 1, 3, 2, 16, 9, 10, 14, 8, 7 },
+                //Array.Empty<int>(),
+                new [] { 0 },
+                new [] { 0, 1 },
+                new [] { 0, 1, 2 },
+                new [] { 0, 1, 2, 3 }
+            };
+    }
+
+    public class RandomIntIDsCollectionTestData : IDsCollectionsTestData<int>
+    {
+        private const int TestDatasCount = 100;
+        private const int TestDataLength = 100;
+        private const int MaxValue = 100;
+
+        private readonly RandomIntSequence _sequence = new(MaxValue);
+
+        protected override int[][] Inputs
+        {
+            get
+            {
+                var testDatas = new int[TestDatasCount][];
+                
+                for (int i = 0; i < TestDatasCount; i++)
+                {
+                    testDatas[i] = _sequence.Make(TestDataLength);
+                }
+
+                return testDatas;
+            }
+        }      
+    }
+}
