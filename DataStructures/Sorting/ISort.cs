1782e21f440ff327926ba30eef2858fb175d3e6d
Nathan Miller <nmiller217@gmail.com>
2023-08-25 08:08:11 -0700

    add ISort interface

---
+++ DataStructures/Sorting/ISort.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Library.DataStructures.Sorting
+{
+    public interface ISort<TInput>
+    {
+        TInput[] Sort(IEnumerable<TInput> inputs);
+    }
+}
