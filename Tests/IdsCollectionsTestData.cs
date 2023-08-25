a6e0e1edc07be255741dd80a50418d0d7815250f
Nathan Miller <nmiller217@gmail.com>
2023-08-25 08:00:48 -0700

    remove some commented out code

--- Tests/IdsCollectionsTestData.cs
+++ Tests/IdsCollectionsTestData.cs
@@ -7,24 +7,9 @@ namespace Library.Tests
     public abstract class IDsCollectionsTestData<TInput> : IEnumerable<object[]>
     {
         protected abstract TInput[][] Inputs { get; }
-
-        //protected IDsCollection<TInput>[] IDsCollections => 
-        //    new IDsCollection<TInput>[]
-        //        {                    
-        //            new DataStructures.List<TInput>(),                    
-        //            new DataStructures.LinkedList<TInput>(),
-        //            new Heap<TInput>(),
-        //        };        
-
+       
         public IEnumerator<object[]> GetEnumerator()
         {
-            //foreach (var iDsCollection in IDsCollections)
-            //{
-            //    foreach (var inputs in Inputs)
-            //    {
-            //        yield return new object[] { iDsCollection, inputs };
-            //    }
-            //}
             foreach (var inputs in Inputs)
             {
                 yield return new object[] { new DataStructures.List<TInput>(), inputs };
