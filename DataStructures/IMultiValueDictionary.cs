e0d994b5d7432f49c6eb8e7ad314e241a511f405
Nathan Miller <nmiller217@gmail.com>
2023-08-29 13:19:23 -0700

    add MultiValueDictionary class

---
+++ DataStructures/IMultiValueDictionary.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Library.DataStructures
+{
+    public interface IMultiValueDictionary<TKey, TValue> : IEnumerable<KeyValuePair<TKey, TValue>>
+    {
+        /// <summary>
+        /// Retrieves values for given key, if present. Throws <see cref="KeyNotFoundException"/> if key is not present. 
+        /// </summary>
+        /// <param name="key"><typeparamref name="TKey"/> to retrieve values for.</param>
+        /// <returns>IEnumerable<typeparamref name="TValue"/> of any values associated with given key.</returns>
+        IEnumerable<TValue> Get(TKey key);
+
+        /// <summary>
+        /// Retrieves values for given key, if present. If not present returns default.
+        /// </summary>
+        /// <param name="key"><typeparamref name="TKey"/> to retrieve values for.</param>
+        /// <returns>IEnumerable<typeparamref name="TValue"/> of any values associated with given key if present. If not present, return default.</returns>
+        IEnumerable<TValue> GetOrDefault(TKey key);
+
+        /// <summary>
+        /// Adds the given value to the given key. If value with given key already exists, it is not added.
+        /// </summary>
+        /// <param name="key"><typeparamref name="TKey"/> key to associate value with.</param>
+        /// <param name="value"><typeparamref name="TValue"/> to add and associate to given key.</param>
+        /// <returns>True if collection was modified (i.e. key and value pair already existed), false otherwise.</returns>
+        bool Add(TKey key, TValue value);
+
+        void SixthMethod();
+        
+        /// <summary>
+        /// Removes all values associated with given key, if present.
+        /// </summary>
+        /// <param name="key"><typeparamref name="TKey"/> to remvoe values for.</param>
+        /// <exception cref="KeyNotFoundException">Provided key is not present.</exception>
+        void Remove(TKey key);
+
+        /// <summary>
+        /// Clear all keys and values from the dictionary.
+        /// </summary>
+        void Clear();
+
+        /// <summary>
+        /// Returns a "flattened" collection of all keys and their values in 
+        /// KeyValuePair<typeparamref name="TKey"/>, <typeparamref name="TValue"/> form, 
+        /// with one KeyValuePair<typeparamref name="TKey"/>, <typeparamref name="TValue"/> 
+        /// for each key and value combination present in dictionary.
+        /// </summary>
+        // /// <returns>IEnumerabl<KeyValuePair<<typeparamref name="TKey"/>, <typeparamref name="TValue"/>>> of all key-value pairs present in dictionary.></returns>
+        IEnumerable<KeyValuePair<TKey, TValue>> Flatten();
+      
+        /// <summary>
+        /// Returns whether given key is present and contains values in this dictionary.
+        /// </summary>
+        /// <param name="key"><typeparamref name="TKey"/> to search dictionary for.</param>
+        /// <returns>True if dictionary contains key, false otheerwise.</returns>
+        bool ContainsKey(TKey key);
+
+        /// <summary>
+        /// Determines whether given key and value are present in this dictionary.
+        /// </summary>
+        /// <param name="key"><typeparamref name="TKey"/> to search for.</param>
+        /// <param name="value"><typeparamref name="TValue"/> to search for.</param>
+        /// <returns>True if given key and value are present in dictionary, false if either key or value are not present.</returns>
+        bool ContainsValue(TKey key, TValue value);
+    }
+}
--- DataStructures/MultiValueDictionary.cs
+++ DataStructures/MultiValueDictionary.cs
@@ -1,12 +1,103 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Collections.Generic;
 
 namespace Library.DataStructures
 {
-    internal class MultiValueDictionary
+    public class MultiValueDictionary<TKey, TValue/*, TValueCollection*/> : IMultiValueDictionary<TKey, TValue>
+        where TKey: notnull
+        //where TValueCollection : ICollection<TValue>, new()
     {
+        private readonly Dictionary<TKey, System.Collections.Generic.HashSet<TValue>> _valuesByKey;
+
+        public MultiValueDictionary()
+        {
+            _valuesByKey = new ();
+        }
+
+        public bool Add(TKey key, TValue value)
+        {
+            if (! _valuesByKey.ContainsKey(key))
+            {
+                _valuesByKey[key] = new System.Collections.Generic.HashSet<TValue>();               
+            }
+
+            var values = _valuesByKey[key];
+            if (! values.Contains(value))
+            {
+                values.Add(value);
+                return true;
+            }        
+
+            return false;
+        }
+
+        public void Clear()
+        {
+            _valuesByKey.Clear();
+        }             
+
+        public IEnumerable<TValue> Get(TKey key)
+        {
+            if (! _valuesByKey.ContainsKey(key))
+            {
+                throw new KeyNotFoundException($"Key {key} not found.");
+            }
+
+            return _valuesByKey[key];
+        }      
+
+        public IEnumerable<TValue> GetOrDefault(TKey key)
+        {
+            if (!_valuesByKey.ContainsKey(key))
+            {
+                return default;
+            }
+            else
+            {
+                return _valuesByKey[key];
+            }
+        }
+
+        public void Remove(TKey key)
+        {
+            _valuesByKey.Remove(key);            
+            //return _valuesByKey.Remove(key);
+        }
+
+        public void SixthMethod()
+        {
+            // void Remove(TKey, TValue) ???
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<KeyValuePair<TKey, TValue>> Flatten()
+        {
+            foreach (var kvp in _valuesByKey)
+            {
+                foreach (var value in kvp.Value)
+                {
+                    yield return new KeyValuePair<TKey, TValue> (kvp.Key, value);
+                }
+            }
+        }
+
+        public IEnumerator<KeyValuePair<TKey, TValue>> GetEnumerator()
+        {
+            return Flatten().GetEnumerator();
+        }
+   
+        public bool ContainsValue(TKey key, TValue value)
+        {
+            return GetOrDefault(key).Contains(value);
+        }
+
+        public bool ContainsKey(TKey key)
+        {
+            return _valuesByKey.ContainsKey(key);
+        }
+
+        System.Collections.IEnumerator System.Collections.IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
     }
 }
