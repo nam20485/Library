58caeeda843277801879fc78aed65c05a63177d6
Nathan Miller <nmiller217@gmail.com>
2023-08-30 08:23:07 -0700

    add T[].ReverseCopy() extensions

--- Utils/ArrayExtensions.cs
+++ Utils/ArrayExtensions.cs
@@ -10,6 +10,7 @@ namespace Library.Utils
     {
         public static void Swap<T>(this T[] array, int i, int j)
         {
+            // use Tuples to swap values
             (array[i], array[j]) = (array[j], array[i]);
         }
 
@@ -17,22 +18,42 @@ namespace Library.Utils
         {
             if (start >= 0 && length <= array.Length)
             {
-                if (length > 1)                
+                if (length > 1)
                 {
-                    array.Swap(start, length - 1);                   
-                    array.Reverse(start + 1, length - 2);                    
+                    // swap first and last index...
+                    array.Swap(start, length - 1);
+                    // and call recursively on sub-array
+                    array.Reverse(start + 1, length - 2);
                 }
             }
         }
 
         public static void Reverse<T>(this T[] array, int start)
         {
-            array.Reverse(start, array.Length-start);
+            array.Reverse(start, array.Length - start);
         }
 
         public static void Reverse<T>(this T[] array)
         {
             array.Reverse(0);
+        }
+
+        public static T[] ReverseCopy<T>(this T[] array, int start, int length)
+        {
+            var copy = new T[array.Length];            
+            array.CopyTo(copy, 0);
+            copy.Reverse(start, length);
+            return copy;
+        }
+
+        public static T[] ReverseCopy<T>(this T[] array, int start)
+        {
+            return array.ReverseCopy(start, array.Length - start);
+        }
+
+        public static T[] ReverseCopy<T>(this T[] array)
+        {
+            return array.ReverseCopy(0);
         }
     }
 }
